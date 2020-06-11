cf842fe3b743ff12caedc151afc03d07d225401f
bmagee2 <bamagee2@gmail.com>
2020-06-11 13:48:15 -0400

    no error

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Navbar from './components/Navbar/Navbar';
-import Auth from './components/Navbar/Auth/Auth';
+
 
 function App() {
   return (
     <div className="App">
       <h1>react react</h1>
-      <Navbar />
-      <Auth />
+      
     </div>
   );
 }
