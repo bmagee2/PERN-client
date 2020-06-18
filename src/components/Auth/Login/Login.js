36025482723e46832561c94ccc4a514d5000f6db
bmagee2 <bamagee2@gmail.com>
2020-06-18 14:33:41 -0400

    props

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -3,19 +3,19 @@ import APIURL from '../../../helpers/environment';      // import environment.js
 
 const Login = (props) => {
 
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+    // const[email, setEmail] = useState('');
+    // const[password, setPassword] = useState('');
 
 
     return(
         <div>
             <label htmlFor="email">Email:</label> 
             <br/>
-            <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" id="email" value={props.email} onChange={(e) => props.setEmail(e.target.value)} />
             <br/>
             <label htmlFor="password">Password:</label>
             <br/>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" id="password" value={props.password} onChange={(e) => props.setPassword(e.target.value)} />
             <br/>
         </div>
     )
